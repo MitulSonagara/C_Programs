f0bb4ddf5ded880dc1b1b126a067f6c3cf028c2b
Mitul Sonagara <95460188+MitulSonagara@users.noreply.github.com>
2022-08-27 13:26:31 +0530

    Reverse the given number

---
+++ ReverseNumber.c
@@ -0,0 +1,17 @@
+/*To reverse a given set of numbers.*/
+
+#include <stdio.h>
+int main()
+{
+    int n, rev = 0, remainder;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    while (n != 0)
+    {
+        remainder = n % 10;
+        rev = rev * 10 + remainder;
+        n = n / 10;
+    }
+    printf("Reversed number = %d", rev);
+    return 0;
+}

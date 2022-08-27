8181d1e5c742e7c87ed19e091968607f9282bab2
Mitul Sonagara <95460188+MitulSonagara@users.noreply.github.com>
2022-08-27 12:00:51 +0530

    Add files via upload
    Fibonacci Series of entered number of terms

---
+++ fibonacci.c
@@ -0,0 +1,27 @@
+/*Write a code to find the fibonacci series of entered terms*/
+
+#include <stdio.h>
+
+int fibonacci(int n)
+{
+    if (n == 1 || n == 2)
+    {
+        return n - 1;
+    }
+    else
+    {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter the number of terms of fibonacci series:");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d ", fibonacci(i));
+    }
+    return 0;
+}
